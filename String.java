0e706d1c19f236e1efd263dcaf2f0605c6d1a1ad
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-03-06 12:52:25 -0800

    Create String.java

---
+++ String.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String message = "   Hello World" + "!!";
+        System.out.println(message.replace("!", "*"));
+        System.out.println(message.trim());
+    }
+}
