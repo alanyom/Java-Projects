0462da40961e84d8a27d6e4839de0dfb58760876
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-03-06 12:40:04 -0800

    Create Encapsulation.java

---
+++ Encapsulation.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        var employee = new Employee();
+        employee.baseSalary = 50_000;
+        employee.hourlyRate = 20;
+        int wage = employee.calculateWage(10);
+        System.out.println(wage);
+    }
+
+    public static int calculateWage(
+            int baseSalary,
+            int extraHours,
+            int hourlyRate
+    ) {
+        return baseSalary + (extraHours * hourlyRate);
+    }
+}
