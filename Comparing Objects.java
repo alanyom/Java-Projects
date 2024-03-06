8fd0890fb6e01cd6751a8605a7af964b851a52e4
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-03-06 12:47:25 -0800

    Create Comparing Objects.java

---
+++ Comparing Objects.java
@@ -0,0 +1,36 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        var point1 = new Point(1, 2);
+        var point2 = new Point(1, 2);
+        System.out.println(point1.hashCode());
+        System.out.println(point2.hashCode());
+    }
+}
+
+
+
+import java.util.Objects;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
