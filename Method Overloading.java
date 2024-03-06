e8ccda9f3a3ea8918deb00f3f949595804b03208
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-03-06 12:42:19 -0800

    Create Method Overloading.java

---
+++ Method Overloading.java
@@ -0,0 +1,30 @@
+//don't overload too many methods because it can get messy
+//overloading is not necessary
+
+
+public class Employee {
+    private int baseSalary;
+    private int hourlyRate;
+
+    public Employee(int baseSalary, int hourlyRate) {
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+    }
+
+    public int calculateWage(int extraHours) {
+        return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public int calculateWage() {
+        return calculateWage(0);
+    }
+
+    private void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less");
+        this.baseSalary = baseSalary;
+    }
+
+    private int getBaseSalary() {
+        return baseSalary;
+    }
