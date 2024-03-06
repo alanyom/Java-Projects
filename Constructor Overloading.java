c6cf2a98f8871f20f8a1a90ee999f22c5ed66d3f
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-03-06 12:44:15 -0800

    Create Constructor Overloading.java

---
+++ Constructor Overloading.java
@@ -0,0 +1,62 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Employee(50_000);
+        var employee = new Employee(50_000, 20);
+        int wage = employee.calculateWage();
+        System.out.println(wage);
+    }
+
+    public static int calculateWage(
+            int baseSalary,
+            int extraHours,
+            int hourlyRate
+    ) {
+        return baseSalary + (extraHours * hourlyRate);
+    }
+}
+
+
+
+public class Employee {
+    private int baseSalary;
+    private int hourlyRate;
+
+    public Employee(int baseSalary) {
+        this(baseSalary, 0);
+    }
+
+    public Employee(int baseSalary, int hourlyRate) {
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+    }
+
+    public int calculateWage(int extraHours) {
+        return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public int calculateWage() {
+        return calculateWage(0);
+    }
+
+    private void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less");
+        this.baseSalary = baseSalary;
+    }
+
+    private int getBaseSalary() {
+        return baseSalary;
+    }
+
+    private void setHourlyRate(int hourlyRate) {
+        if (hourlyRate < 0)
+            throw new IllegalArgumentException("Hourly rate cannot be 0 or less");
+        this.hourlyRate = hourlyRate;
+    }
+
+    private int getHourlyRate() {
+        return hourlyRate;
+    }
+}
