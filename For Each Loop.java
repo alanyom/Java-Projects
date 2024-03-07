b67dedd44cb82d5c2a437004f05d4896a7754876
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-03-07 08:52:01 -0800

    Create For Each Loop.java

---
+++ For Each Loop.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Mango", "Orange"};
+        
+        for (String fruit : fruits)
+            System.out.println(fruit);
+    }
+}
