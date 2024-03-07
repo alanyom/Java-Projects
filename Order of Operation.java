53f1c6df7385bc6182607da2dd7ca6768342a8ee
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-03-07 08:43:24 -0800

    Create Order of Operation.java

---
+++ Order of Operation.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 10 + 3 * 2;
+        System.out.println(x); //This result is 16 because java has order of operation
+    }
+}
