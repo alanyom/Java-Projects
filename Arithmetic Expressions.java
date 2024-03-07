83a910c734a2883ba9f29cb23d0788e59f0a7ccc
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-03-07 08:31:15 -0800

    Create Arithmetic Expressions.java

---
+++ Arithmetic Expressions.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double result = (double)10 / (double)3;
+        int x = 1;
+        x += 2;
+        System.out.println(x); //This result is 3
+        System.out.println(result); //This result is 3.3333
+    }
+}
