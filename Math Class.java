f70c4afdc7243901d73a181e7987a33906a44bda
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-03-07 08:43:48 -0800

    Create Math Class.java

---
+++ Math Class.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double result = (int)(Math.random() * 100);
+        System.out.println(result);
+    }
+}
