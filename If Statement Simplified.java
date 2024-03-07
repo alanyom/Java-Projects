495bc91c9796bbb74d81e5b38bd59d523c7c6516
Alan Yom <89889751+alanyom@users.noreply.github.com>
2024-03-07 08:48:36 -0800

    Create If Statement Simplified.java

---
+++ If Statement Simplified.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int income = 120_000;
+        boolean hasHighIncome = (income > 100_000);
+    }
+}
